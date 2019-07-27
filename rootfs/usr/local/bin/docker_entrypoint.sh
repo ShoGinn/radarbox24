0438dff71e4d514b27c6c133496f7e0c83c49080
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-07-26 17:58:39 -0700

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -15,7 +15,7 @@ sleep 5s
 exec /usr/bin/rbfeeder \
   --set-key ${RADARBOX24_KEY} \
   --set-network-mode on \
-  --set-network-host $(getent hosts ${DUMP1090_HOST} | head -n 1 | awk '{print $1}') \
+  --set-network-host $(getent hosts ${DUMP1090_SERVER} | head -n 1 | awk '{print $1}') \
   --set-network-port ${DUMP1090_PORT} \
   --set-network-protocol ${DUMP1090_PROTOCOL:-beast} \
   ${@}
